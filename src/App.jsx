bc2a1e91a0ff6199d892ac3bccf4d3039fe30bff
qpwoei405 <qpwoei4051@gmail.com>
2026-05-15 13:33:23 +0900

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const soundFiles = {
   standard: "/sounds/standard.mp3",
